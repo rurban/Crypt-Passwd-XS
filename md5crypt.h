82bac3ce85cddcbff7803478dfce67ffbb0af39b
John Lightsey <jd@cpanel.net>
2010-10-05 10:16:51 -0500

    Add header for md5crypt

---
+++ md5crypt.h
@@ -0,0 +1,18 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@login.dknet.dk> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ *
+ * $FreeBSD: src/lib/libcrypt/crypt-md5.c,v 1.5.2.1 2001/05/24 12:20:02 markm Exp $
+ * $DragonFly: src/lib/libcrypt/crypt-md5.c,v 1.3 2005/08/04 17:27:09 drhodus Exp $
+ */
+
+#ifndef _MD5CRYPT_H
+#define _MD5CRYPT_H
+
+char *crypt_md5(const char *pw, const char *salt);
+
+#endif /* _MD5CRYPT_H */
