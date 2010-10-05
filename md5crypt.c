a9959891b0a0bac91dbe7258258c4044018a914a
John Lightsey <jd@cpanel.net>
2010-10-05 10:11:46 -0500

    Move md5 C code to base directory Remove main block from md5crypt.c Remove md5.copyright (already included in md5.c comments

--- md5crypt.c
+++ md5crypt.c
@@ -133,14 +133,3 @@ crypt_md5(const char *pw, const char *salt)
 
 	return passwd;
 }
-
-int main(int argc, char **argv) {
-  const char *password = "test1234";
-  const char *salt = "12345";
-  const char *expected = "$1$12345$8R/aJ9geL80VC/tEqvbQ01";
-  char * got;
-
-  got = crypt_md5(password, salt);
-  printf("Got: %s\nExpected: %s\n", got, expected);
-  return 0;
-}
